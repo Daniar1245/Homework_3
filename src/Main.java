515e949b0230fc2156b6df85b933bf41bab31ae7
Daniar1245 <daniarraimkulov123@gmail.com>
2022-08-06 17:43:27 +0600

    RD

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         double[] doubleNumbers = {12.86, -32.45, 23.00, 65.78, -75.90, 56.78, 34.67, -46.76, 33.44, 89.71, -100.00, 56.00, -67.78, 54.98, 0.96};
-        double point = 0;
-        if (doubleNumbers.length > 0) {
-            double summa = 0;
-            for (double doubleNumber : doubleNumbers) {
+        double numbers = 0;
+        int summa = 0;
+        boolean fG = false;
+        for (double doubleNumber : doubleNumbers) {
+            if (doubleNumber < 0) {
                 summa += doubleNumber;
+                fG = true;
+            } else if (fG) {
+                summa++;
+                numbers += doubleNumber;
             }
-            point = summa / doubleNumbers.length;
-            System.out.println(point);
         }
+        System.out.println(numbers / summa);
     }
 }
